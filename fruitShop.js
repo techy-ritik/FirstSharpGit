b4bb30703aaef0f589a4e7e75be844e51107272f
techy-ritik <ritikeshjee@gmail.com>
2024-01-08 12:46:22 +0530

    FruitShopAssignment

---
+++ fruitShop.js
@@ -0,0 +1,13 @@
+const mainHeading=document.getElementById('main-heading');
+mainHeading.textContent = 'Fruit World';
+mainHeading.style.color= 'orange';
+
+const Header = document.getElementById('header');
+Header.style.backgroundColor='green';
+Header.style.borderBottom='3px solid orange';
+
+const basketHeading=document.getElementById('basket-heading');
+basketHeading.style.color='green';
+
+const thanks=document.getElementById('thanks');
+thanks.innerHTML='<p>Please visit us again</p>';
