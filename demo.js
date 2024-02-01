fb7b4291d075ca34708bc83e9b2d500033cd7e70
techy-ritik <ritikeshjee@gmail.com>
2024-02-01 20:00:11 +0530

    till task16 of adv js

--- demo.js
+++ demo.js
@@ -1,3 +1,24 @@
-console.log('Jai Sri Ram')
-console.log('jai HAnuman')
+console.log('jai Hanuman')
+
+// const h1=document.getElementById('ram');
+// h1.style.color='orange';
+// h1.style.fontStyle='italic';
+// h1.style.backgroundColor='green';
+
+// function hanuman(){
+    
+// }
+
+
+console.log('a');
+
+console.log('b');
+
+
+
+setTimeout(()=> console.log('c'), 1000);
+
+
+
+console.log('d');
 
