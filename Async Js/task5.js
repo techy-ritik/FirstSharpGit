3fb55aac4aceb3582b5312993f956e205272c285
techy-ritik <ritikeshjee@gmail.com>
2024-02-09 22:52:17 +0530

    upto asysnc js task5

---
+++ Async Js/task5.js
@@ -0,0 +1,14 @@
+// Write your code here:
+function buyBike(callback){
+    setTimeout(()=>{
+       console.log("Bought Royal Enfield Himalayan");
+    },2000);
+    callback();
+}
+function planTrip(){
+    setTimeout(()=>{
+        console.log("Trip to Ladakh planned");
+    },1000);
+    // callback();
+}
+buyBike(planTrip);
